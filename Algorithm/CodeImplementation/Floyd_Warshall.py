da879d13754ab24da43842453c544bb12586db1e
xilucks <siun0331@gmail.com>
2022-04-11 12:05:16 +0900

    플로이드 와샬 알고리즘 구현

---
+++ Algorithm/CodeImplementation/Floyd_Warshall.py
@@ -0,0 +1,19 @@
+INF = int(1e9)
+
+n, m = map(int, input().split())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n+1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
